86aae6c697bce8111d510a344e33e28a71c13efe
Usuario <Usuario@10.22.148.83>
2018-09-20 13:14:47 +0100

    Kata1 version 3 donde empleamos LocalDate para almacenar la fecha y Period para calcular la edad

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,14 +1,13 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
    private final String name;
-   private final Calendar birthdate;
+   private final LocalDate birthdate;
    private final long MILLIS_PER_YEAR = (long) (1000*60*60*24*365.25);
-    public Person(String name, Calendar birthdate) {
+    public Person(String name, LocalDate birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -17,16 +16,14 @@ public class Person {
         return name;
     }
 
-    public Calendar getBirthdate() {
+    public LocalDate getBirthdate() {
         return birthdate;
     }
    
    public int getAge(){
-       Calendar today = GregorianCalendar.getInstance();
-       return (int) (milliSecondsToYear(today.getTimeInMillis() - birthdate.getTimeInMillis()));
+       LocalDate today = LocalDate.now();
+       Period p = Period.between(birthdate, today);
+       return (int) (p.getYears());
    }
-   
-   private long milliSecondsToYear(long milles){
-       return milles / MILLIS_PER_YEAR;
-   }
+    
 }
